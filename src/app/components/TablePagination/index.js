da642e8f2f5d10383e9754bc7f5f4f4f4779ff9b
Rafael Vieira <rafaellvs322@gmail.com>
2020-08-14 03:52:53 -0300

    ENH: update Pagination to cover for fullData changes

--- src/app/components/TablePagination/index.js
+++ src/app/components/TablePagination/index.js
@@ -7,27 +7,29 @@ import Navigation from './Navigation'
 
 import { Container } from './styled'
 
-const Pagination = ({ response, setData }) => {
+const TablePagination = ({ fullData, setData }) => {
   const [selected, setSelected] = useState(0)
 
   const ITEMS_TO_SHOW = 25
-  const totalPages = Math.ceil(response.length / ITEMS_TO_SHOW)
+  const totalPages = Math.ceil(fullData.length / ITEMS_TO_SHOW)
   const sliceStart = selected * ITEMS_TO_SHOW
   const sliceEnd =
     selected === totalPages - 1
-      ? sliceStart + (response.length % ITEMS_TO_SHOW)
+      ? sliceStart + (fullData.length % ITEMS_TO_SHOW)
       : sliceStart + ITEMS_TO_SHOW
 
   useEffect(() => {
-    setData(
-      response.slice(sliceStart, sliceEnd)
-    )
-  }, [selected])
+    setData(fullData.slice(sliceStart, sliceEnd))
+  }, [selected, fullData])
+
+  useEffect(() => {
+    setSelected(0)
+  }, [fullData])
 
   return (
     <Container>
-      <Text align='center'>
-        {`${sliceStart}-${sliceEnd} / ${response.length}`}
+      <Text align='center' padding='0 0 .5rem 0'>
+        {`${sliceStart}-${sliceEnd} / ${fullData.length}`}
       </Text>
 
       <Navigation
@@ -39,9 +41,9 @@ const Pagination = ({ response, setData }) => {
   )
 }
 
-Pagination.propTypes = {
-  response: PropTypes.array,
+TablePagination.propTypes = {
+  fullData: PropTypes.array,
   setData: PropTypes.func,
 }
 
-export default Pagination
+export default TablePagination
