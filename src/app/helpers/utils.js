b45a586339fa9cc74be3f7b3498c0ddde09cfd9c
Rafael Vieira <rafaellvs322@gmail.com>
2020-08-16 11:13:57 -0300

    ENH: add formatAttribute special cases

--- src/app/helpers/utils.js
+++ src/app/helpers/utils.js
@@ -5,5 +5,9 @@ export const capitalizeString = string =>
   string.charAt(0).toUpperCase() + string.slice(1)
 
 // format thead attributes
-export const formatAttribute = string =>
-  capitalizeString(string.replace(/_/, ' '))
+export const formatAttribute = string => {
+  if (string === 'required_level') return 'Level'
+  if (string === 'required_vocation') return 'Vocation'
+
+  return capitalizeString(string.replace(/_/, ' '))
+}
